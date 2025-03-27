f6c8c924e0b400b134a984eff442a17aaf95cdb5
Silent Sky(Phillip Johnerud) <88206755+miomip@users.noreply.github.com>
2025-03-27 09:55:29 +0100

    Lagt til metadata, gjort slik at bilde ikke laster før den er på skjermen. Optimalisert css, js, og fonts. Forbedret responsivitet noe.
    Signed-off-by: Silent Sky(Phillip Johnerud) <88206755+miomip@users.noreply.github.com>

--- Scripter/main.js
+++ Scripter/main.js
@@ -1,22 +1,10 @@
+import {styles as typescaleStyles} from '@material/web/typography/md-typescale-styles.js';
+document.adoptedStyleSheets.push(typescaleStyles.styleSheet);
+
 const anchorEl = document.body.querySelector('#usage-anchor');
 const menuEl = document.body.querySelector('#usage-menu');
 
 anchorEl.addEventListener('click', () => { menuEl.open = !menuEl.open; });
-
-
-
-const menu = document.querySelector('#mobile-menu');
-const menuLinks = document.querySelector('.navbar__menu');
-
-menu.addEventListener('click', function() { // Korrigert fra 'clik' til 'click'
-    menu.classList.toggle('is-active');
-    menuLinks.classList.toggle('active');
-});
-
-function setFontSize(size) {
-    localStorage.setItem('fontSize', size);
-    document.body.style.fontSize = size + 'px';
-}
 
 
 
